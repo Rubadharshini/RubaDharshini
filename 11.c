ce40618b3c88a052d1aca75928c813db451a43c9
Rubadharshini <rubadharshini51@gmail.com>
2016-07-10 12:05:16 +0530

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int aa,p,i,j,c=0;
+clrscr();
+printf("\n Enter the number of students:");
+scanf("%d",&a);
+p=2*aa;
+for(i=0;i<p;i++)
+{
+for(j=i+1;j<=p;j++)
+ {
+ printf("\n%d\t%d\n",i,j);
+ c++;
+ }  }
+ printf("the daily  combination is %d",count);
+ getch();
+}
